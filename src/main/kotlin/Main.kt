241c87eea750fa5f60c7b258a67f2efeeb515fee
ana7293 <ana7293nik@gmail.com>
2023-06-10 02:03:02 +0500

    Exercise2/3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,29 @@
 fun main(args: Array<String>) {
+// ФУНКЦИИ
+  /* val result: Int = multiplay(9, 6)
+    println(result)
+    fun multiplay (a: Int, b: Int) : Int {
+     return a * b
+    }
+   - в одно строку можно так - fun multiplay (a: Int, b: Int) = a * b
+    */
 
-    val firstName: String = "Анастасия"
-    val lastName: String  = "Николаева"
-    var height: Double = 1.23
-    var weight: Float = 34F
-    val isChild: Boolean = height < 150 && weight < 40
+    val anna = 10000         //создадим функцию для расчета накоплений
+    val nata = 20000
+    val lena = 50000
 
-    var info = "$firstName $lastName, $height, $weight, $isChild"
-    println(info)
+    var sum =  bankCalculate(anna, 0.25, 6) +
+            bankCalculate(nata, 0.5, 3) +
+            bankCalculate(lena, 0.75, 2)
+
+   println("Все вместе могут накопить: $sum")
+
+}
+
+fun bankCalculate (salary: Int, rate: Double, mounth: Int): Double {
+    var result = salary * 0.87 * rate * mounth
+    println("С зарплатой $salary за $mounth месяцев при откладывании $rate % накопится $result")
+    return result
 
 
 }
